76e56cbec73a6b6d52449cc5222529bd894966ca
ianoflynnautomation <ian.oflynn11@gmail.com>
2023-02-27 20:10:20 +0100

    New: Solution 206

--- src/LeetCode.Solutions/21_MergeTwoSortedLists.cs
+++ src/LeetCode.Solutions/21_MergeTwoSortedLists.cs
@@ -41,16 +41,4 @@ namespace LeetCode.Solutions
             return dummyHead.next;
         }
     }
-
-    //Definition for singly-linked list.
-    public class ListNode
-    {
-        public int val;
-        public ListNode? next;
-        public ListNode(int val = 0, ListNode? next = null)
-        {
-            this.val = val;
-            this.next = next;
-        }
-    }
 }
---
+++ src/LeetCode.Tests/_206_ReverseLinkedList_Tests.cs
@@ -0,0 +1,45 @@
+
+namespace LeetCode.Tests
+{
+    public class _206_ReverseLinkedList_Tests
+    {
+        [Test]
+        public void Case1()
+        {
+            var head = TestsHelper.GenerateListNodeList(new int[] { 1, 2, 3, 4, 5 });
+
+            var solution = new _206_ReverseLinkedList();
+            var result = solution.ReverseList(head);
+
+            var expectedResult = TestsHelper.GenerateListNodeList(new int[] { 5, 4, 3, 2, 1 });
+
+            result.Should().BeEquivalentTo(expectedResult);
+        }
+
+        [Test]
+        public void Case2()
+        {
+            var head = TestsHelper.GenerateListNodeList(new int[] { 1, 2 });
+
+            var solution = new _206_ReverseLinkedList();
+            var result = solution.ReverseList(head);
+
+            var expectedResult = TestsHelper.GenerateListNodeList(new int[] { 2, 1 });
+
+            result.Should().BeEquivalentTo(expectedResult);
+        }
+
+        [Test]
+        public void Case3()
+        {
+            var head = TestsHelper.GenerateListNodeList(new int[] { });
+
+            var solution = new _206_ReverseLinkedList();
+            var result = solution.ReverseList(head);
+
+            var expectedResult = TestsHelper.GenerateListNodeList(new int[] { });
+
+            result.Should().BeEquivalentTo(expectedResult);
+        }
+    }
+}
