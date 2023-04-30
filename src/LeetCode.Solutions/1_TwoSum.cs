f50438a2506fa73d5478de6e0c927aedc4b1c52b
ianoflynnautomation <ian.oflynn11@gmail.com>
2023-04-30 12:05:03 +0200

    New: Solution 1

---
+++ src/LeetCode.Solutions/1_TwoSum.cs
@@ -0,0 +1,22 @@
+
+namespace LeetCode.Solutions
+{
+    public class _1_TwoSum
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            var dic = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (dic.ContainsKey(target - nums[i]))
+                    return new int[] { i, dic[target - nums[i]] };
+                else if (!dic.ContainsKey(nums[i]))
+                    dic.Add(nums[i], i);
+            }
+
+            return new int[2];
+        }
+
+    }
+}
---
+++ src/LeetCode.Tests/1_TwoSum_Tests.cs
@@ -0,0 +1,37 @@
+
+namespace LeetCode.Tests
+{
+    public class _1_TwoSum_Tests
+    {
+        [Test]
+        public void Case1()
+        {
+            var solution = new _1_TwoSum();
+            var result = solution.TwoSum(new int[] { 2, 7, 11, 15 }, 9);
+
+            result.Should().BeEquivalentTo(new int[] { 0, 1 });
+
+        }
+
+        [Test]
+        public void Case2()
+        {
+            var solution = new _1_TwoSum();
+            var result = solution.TwoSum(new int[] { 3, 2, 4 }, 6);
+
+            result.Should().BeEquivalentTo(new int[] { 2, 1 });
+
+        }
+
+        [Test]
+        public void Case3()
+        {
+            var solution = new _1_TwoSum();
+            var result = solution.TwoSum(new int[] { 3, 3 }, 6);
+
+            result.Should().BeEquivalentTo(new int[] { 1, 0 });
+
+        }
+
+    }
+}
