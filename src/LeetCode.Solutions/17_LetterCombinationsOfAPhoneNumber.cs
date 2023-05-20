82036707e6c9860d6b02c32a45a199fb0eaf7255
ianoflynnautomation <ian.oflynn11@gmail.com>
2023-05-20 21:31:09 +0200

    New: Solution 17

---
+++ src/LeetCode.Solutions/17_LetterCombinationsOfAPhoneNumber.cs
@@ -0,0 +1,53 @@
+using System.Text;
+
+namespace LeetCode.Solutions
+{
+    public class _17_LetterCombinationsOfAPhoneNumber
+    {
+        private readonly string[] PhoneDigits = {
+            "",     // 0
+            "",     // 1
+            "abc",  // 2
+            "def",  // 3
+            "ghi",  // 4
+            "jkl",  // 5
+            "mno",  // 6
+            "pqrs", // 7
+            "tuv",  // 8
+            "wxyz"  // 9
+        };
+
+        public IList<string> LetterCombinations(string digits)
+        {
+
+            IList<string> combinations = new List<string>();
+
+            if (digits.Length == 0)
+                return combinations;
+
+            Backtrack(combinations, digits, new StringBuilder(), 0);
+
+            return combinations;
+        }
+
+        private void Backtrack(IList<string> combinations, string digits, StringBuilder current, int index)
+        {
+            if (index == digits.Length)
+            {
+                combinations.Add(current.ToString());
+                return;
+            }
+
+            int digit = int.Parse(digits[index].ToString());
+            string letters = PhoneDigits[digit];
+
+            for (int i = 0; i < letters.Length; i++)
+            {
+                current.Append(letters[i]);
+                Backtrack(combinations, digits, current, index + 1);
+                current.Remove(current.Length - 1, 1);
+            }
+
+        }
+    }
+}
--- src/LeetCode.Tests/Usings.cs
+++ src/LeetCode.Tests/Usings.cs
@@ -3,3 +3,4 @@ global using LeetCode.Solutions;
 global using FluentAssertions;
 global using System.Collections.Generic;
 global using LeetCode.Tests.Enums;
+global using System.Text;
